9a5aa38981f9924d02bf1adb5e492c301d4b2190
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-07-17 16:10:09 +0530

    add user and auth

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@/store/slices/authSlice';
 import taskReducer from '@/store/slices/taskSlice';
+import userReducer from '@/store/slices/userSlice';
+
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         task: taskReducer,
+        user: userReducer,
     },
 });
 
